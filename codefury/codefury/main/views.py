3d586e104b535bd037a5b385c371252ed6cd4022
gin-2710 <89338408+gin-2710@users.noreply.github.com>
2022-08-13 15:51:19 +0530

    added templates
    teplateeee

--- codefury/codefury/main/views.py
+++ codefury/codefury/main/views.py
@@ -1,7 +1,11 @@
 from django.http import HttpResponse
 from django.template import loader
+from quart import template_rendered
+from django.shortcuts import render
+
 
 
 def index(request):
     template = loader.get_template('index.html')
     return HttpResponse(template.render())
+
