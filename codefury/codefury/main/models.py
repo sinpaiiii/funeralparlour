35e1b23576be589b598bf6b9817050ad3b8e19e9
gin-2710 <89338408+gin-2710@users.noreply.github.com>
2022-08-13 14:17:24 +0530

    idk what i did

--- codefury/codefury/main/models.py
+++ codefury/codefury/main/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class startUp(models.Model):
+    # id field is added automatically
+    firstName = models.CharField(max_length=40)
+    lastName = models.CharField(max_length=40)
+    startUpName = models.CharField(max_length=40)
+    desc = models.TextField()
+    funds = models.BooleanField()
+    contactNumber = models.IntegerField()
+    email = models.BooleanField()
+    # images= models.ImageField(upload_to ="images")
